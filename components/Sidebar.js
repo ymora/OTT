a0f4a1279cea873bb30a7f59294204ff3f98fa16
ymora <ymora@free.fr>
2025-11-27 06:17:57 +0100

    fix: ajouter useEffect manquant pour maintenir menu Documentation ouvert

--- components/Sidebar.js
+++ components/Sidebar.js
@@ -1,6 +1,6 @@
 'use client'
 
-import { useMemo, useState } from 'react'
+import { useMemo, useState, useEffect } from 'react'
 import Link from 'next/link'
 import { usePathname, useRouter } from 'next/navigation'
 import { useAuth } from '@/contexts/AuthContext'
