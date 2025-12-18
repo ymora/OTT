9d78339df9c2608ecc27bf4a2feb3eb749683ed0
ymora <ymora@free.fr>
2025-12-18 19:21:30 +0100

    clean: Supprimer fichiers SQL redondants - seul schema.sql nécessaire

--- sql/migration_add_measurements_deleted_at.sql
+++
@@ -1,7 +0,0 @@
--- Migration pour ajouter la colonne deleted_at à la table measurements
--- Permet l'archivage (soft delete) des mesures historiques
-
-ALTER TABLE measurements ADD COLUMN IF NOT EXISTS deleted_at TIMESTAMPTZ;
-
-COMMENT ON COLUMN measurements.deleted_at IS 'Timestamp d''archivage de la mesure (soft delete). NULL = mesure active, non NULL = mesure archivée.';
-
